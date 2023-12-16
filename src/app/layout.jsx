f54f457cc7003aefccacfd8a426df9614ce01b0a
IqbalFebrianwar <zonelyrics26@gmail.com>
2023-12-16 14:51:54 +0700

    Feat : fix bg

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html data-theme="light" lang="en">
+    <html data-theme="light" className="bg-white" lang="en">
       <body className={inter.className}>
         <NavbarComponent />
         {children}
